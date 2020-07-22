8281bfe57d7ddd0bf1b4248b45e99b7d56610590
Matthew Muckley <matt.muckley@gmail.com>
2020-07-22 12:30:42 -0700

    Added FB headers

--- fastmri/__init__.py
+++ fastmri/__init__.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
 from .coil_combine import rss, rss_complex
 from .math import (
     complex_abs,

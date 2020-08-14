4222609d03abbf0cf8f4732a0f94dd52e36b8e7f
Matthew Muckley <matt.muckley@gmail.com>
2020-08-14 07:25:33 -0700

    Add deprecation warning to common

--- common/__init__.py
+++ common/__init__.py
@@ -4,3 +4,8 @@ Copyright (c) Facebook, Inc. and its affiliates.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
+
+import warnings
+
+dep_str = """Importing from common has been deprecated and will be removed in a future update. Please import from fastmri instead."""
+warnings.warn(dep_str)

020711ae981447559b737085ba62d9e9b9722d52
Matthew Muckley <matt.muckley@gmail.com>
2020-10-15 07:31:56 -0700

    Fix for hyperparam logging

--- fastmri/pl_modules/mri_module.py
+++ fastmri/pl_modules/mri_module.py
@@ -79,7 +79,6 @@ class MriModule(pl.LightningModule):
             num_workers (int, default=4): Number of workers for PyTorch dataloader.
         """
         super().__init__()
-        self.save_hyperparameters()
 
         self.data_path = data_path
         self.challenge = challenge

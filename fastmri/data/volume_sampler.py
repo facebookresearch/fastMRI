60a85df3b5e0746faeb3e80d4f4558158f02ab05
Matthew Muckley <matt.muckley@gmail.com>
2020-11-05 20:08:07 -0800

    Remove unused import

--- fastmri/data/volume_sampler.py
+++ fastmri/data/volume_sampler.py
@@ -5,7 +5,6 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-from pathlib import Path
 from typing import List, Optional, Union
 
 import torch

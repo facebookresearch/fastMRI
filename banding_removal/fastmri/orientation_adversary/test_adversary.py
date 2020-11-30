a064326e8e412623591153bbf530b161c43b7664
Matthew Muckley <matt.muckley@gmail.com>
2020-11-30 13:58:15 -0500

    Facebook open source compliance (#113)

--- banding_removal/fastmri/orientation_adversary/test_adversary.py
+++ banding_removal/fastmri/orientation_adversary/test_adversary.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
 import sys
 sys.path.append(sys.path[0] + "/../..")
 

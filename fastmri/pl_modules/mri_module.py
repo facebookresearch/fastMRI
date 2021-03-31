be875f4018a7b1acebf57e2274cab69e8b12e43b
Anton Schwaighofer <antonsc@microsoft.com>
2021-03-31 16:38:17 +0100

    logger fix

--- fastmri/pl_modules/mri_module.py
+++ fastmri/pl_modules/mri_module.py
@@ -9,10 +9,12 @@ import pathlib
 from argparse import ArgumentParser
 from collections import defaultdict
 
-import fastmri
 import numpy as np
 import pytorch_lightning as pl
 import torch
+from torch.utils.tensorboard import SummaryWriter
+
+import fastmri
 from fastmri import evaluate
 
 
@@ -69,13 +71,13 @@ class MriModule(pl.LightningModule):
     def validation_step_end(self, val_logs):
         # check inputs
         for k in (
-            "batch_idx",
-            "fname",
-            "slice_num",
-            "max_value",
-            "output",
-            "target",
-            "val_loss",
+                "batch_idx",
+                "fname",
+                "slice_num",
+                "max_value",
+                "output",
+                "target",
+                "val_loss",
         ):
             if k not in val_logs.keys():
                 raise RuntimeError(
@@ -94,7 +96,7 @@ class MriModule(pl.LightningModule):
         if self.val_log_indices is None:
             self.val_log_indices = list(
                 np.random.permutation(len(self.trainer.val_dataloaders[0]))[
-                    : self.num_log_images
+                : self.num_log_images
                 ]
             )
 
@@ -112,15 +114,19 @@ class MriModule(pl.LightningModule):
                 output = output / output.max()
                 target = target / target.max()
                 error = error / error.max()
-                self.logger.experiment.add_image(
-                    f"{key}/target", target, global_step=self.global_step
-                )
-                self.logger.experiment.add_image(
-                    f"{key}/reconstruction", output, global_step=self.global_step
-                )
-                self.logger.experiment.add_image(
-                    f"{key}/error", error, global_step=self.global_step
-                )
+                experiments = self.logger.experiment if isinstance(self.logger.experiment, list) \
+                    else [self.logger.experiment]
+                for experiment in experiments:
+                    if isinstance(experiment, SummaryWriter):
+                        experiment.add_image(
+                            f"{key}/target", target, global_step=self.global_step
+                        )
+                        experiment.add_image(
+                            f"{key}/reconstruction", output, global_step=self.global_step
+                        )
+                        experiment.add_image(
+                            f"{key}/error", error, global_step=self.global_step
+                        )
 
         # compute evaluation metrics
         mse_vals = defaultdict(dict)
@@ -175,10 +181,10 @@ class MriModule(pl.LightningModule):
 
         # check to make sure we have all files in all metrics
         assert (
-            mse_vals.keys()
-            == target_norms.keys()
-            == ssim_vals.keys()
-            == max_vals.keys()
+                mse_vals.keys()
+                == target_norms.keys()
+                == ssim_vals.keys()
+                == max_vals.keys()
         )
 
         # apply means across image volumes
@@ -194,14 +200,14 @@ class MriModule(pl.LightningModule):
             )
             metrics["nmse"] = metrics["nmse"] + mse_val / target_norm
             metrics["psnr"] = (
-                metrics["psnr"]
-                + 20
-                * torch.log10(
-                    torch.tensor(
-                        max_vals[fname], dtype=mse_val.dtype, device=mse_val.device
-                    )
+                    metrics["psnr"]
+                    + 20
+                    * torch.log10(
+                torch.tensor(
+                    max_vals[fname], dtype=mse_val.dtype, device=mse_val.device
                 )
-                - 10 * torch.log10(mse_val)
+            )
+                    - 10 * torch.log10(mse_val)
             )
             metrics["ssim"] = metrics["ssim"] + torch.mean(
                 torch.cat([v.view(-1) for _, v in ssim_vals[fname].items()])

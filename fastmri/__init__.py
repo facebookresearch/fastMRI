a588fb1b7980175e6fe6b17497148e02daf31c12
Matthew Muckley <matt.muckley@gmail.com>
2021-07-21 13:29:42 -0400

    Increment alpha version (#150)

--- fastmri/__init__.py
+++ fastmri/__init__.py
@@ -5,7 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2a20210721"
 __author__ = "Facebook/NYU fastMRI Team"
 __author_email__ = "fastmri@fb.com"
 __license__ = "MIT"

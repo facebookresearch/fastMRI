dc1f66b1f7675182c6f6633bac772ce6d42dae6b
Timsey <tim_bb@hotmail.com>
2022-01-21 16:25:14 +0100

    fixed imports in adaptive_varnet demo script

--- fastmri_examples/adaptive_varnet/train_adaptive_varnet_demo.py
+++ fastmri_examples/adaptive_varnet/train_adaptive_varnet_demo.py
@@ -18,11 +18,11 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import Callback
 from wandb.util import parse_sweep_id
 
-from fastmri_examples.adaptive_varnet.fastmri.utils import str2bool, int2none, str2none
-from fastmri_examples.adaptive_varnet.fastmri.data.mri_data import fetch_dir
-from fastmri_examples.adaptive_varnet.fastmri.data.subsample import create_mask_for_mask_type
-from fastmri_examples.adaptive_varnet.fastmri.data.transforms import VarNetDataTransform, MiniCoilTransform
-from fastmri_examples.adaptive_varnet.fastmri.pl_modules import FastMriDataModule, VarNetModule, ActiveVarNetModule
+from fastmri.utils import str2bool, int2none, str2none
+from fastmri.data.mri_data import fetch_dir
+from fastmri.data.subsample import create_mask_for_mask_type
+from fastmri.data.transforms import VarNetDataTransform, MiniCoilTransform
+from fastmri.pl_modules import FastMriDataModule, VarNetModule, ActiveVarNetModule
 
 
 def make_wandb_run_name(args):

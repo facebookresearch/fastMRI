69db5c4e49ed0eda93bae8ed4daefacd6b2235b5
Matthew Muckley <matt.muckley@gmail.com>
2022-01-21 13:33:17 -0800

    Revert import

--- fastmri/models/varnet.py
+++ fastmri/models/varnet.py
@@ -6,7 +6,7 @@ LICENSE file in the root directory of this source tree.
 """
 
 import math
-from typing import List, Optional, Tuple
+from typing import List, Tuple, Optional
 
 import fastmri
 import torch

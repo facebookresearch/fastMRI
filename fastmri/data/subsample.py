347ced888281ce24b0de8f97184e04eefc414aef
Matthew Muckley <matt.muckley@gmail.com>
2022-01-21 11:42:26 -0800

    Bad copy/paste

--- fastmri/data/subsample.py
+++ fastmri/data/subsample.py
@@ -1,5 +1,6 @@
 """
 Copyright (c) Facebook, Inc. and its affiliates.
+
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
@@ -31,14 +32,17 @@ def temp_seed(rng: np.random.RandomState, seed: Optional[Union[int, Tuple[int, .
 class MaskFunc:
     """
     An object for GRAPPA-style sampling masks.
+
     This crates a sampling mask that densely samples the center while
     subsampling outer k-space regions based on the undersampling factor.
+
     When called, ``MaskFunc`` uses internal functions create mask by 1)
     creating a mask for the k-space center, 2) create a mask outside of the
     k-space center, and 3) combining them into a total mask. The internals are
     handled by ``sample_mask``, which calls ``calculate_center_mask`` for (1)
     and ``calculate_acceleration_mask`` for (2). The combination is executed
     in the ``MaskFunc`` ``__call__`` function.
+
     If you would like to implement a new mask, simply subclass ``MaskFunc``
     and overwrite the ``sample_mask`` logic. See examples in ``RandomMaskFunc``
     and ``EquispacedMaskFunc``.
@@ -83,11 +87,13 @@ class MaskFunc:
     ) -> Tuple[torch.Tensor, int]:
         """
         Sample and return a k-space mask.
+
         Args:
             shape: Shape of k-space.
             offset: Offset from 0 to begin mask (for equispaced masks). If no
                 offset is given, then one is selected randomly.
             seed: Seed for random number generator for reproducibility.
+
         Returns:
             A 2-tuple containing 1) the k-space mask and 2) the number of
             center frequency lines.
@@ -110,13 +116,16 @@ class MaskFunc:
     ) -> Tuple[torch.Tensor, torch.Tensor, int]:
         """
         Sample a new k-space mask.
+
         This function samples and returns two components of a k-space mask: 1)
         the center mask (e.g., for sensitivity map calculation) and 2) the
         acceleration mask (for the edge of k-space). Both of these masks, as
         well as the integer of low frequency samples, are returned.
+
         Args:
             shape: Shape of the k-space to subsample.
             offset: Offset from 0 to begin mask (for equispaced masks).
+
         Returns:
             A 3-tuple contaiing 1) the mask for the center of k-space, 2) the
             mask for the high frequencies of k-space, and 3) the integer count
@@ -154,11 +163,13 @@ class MaskFunc:
     ) -> np.ndarray:
         """
         Produce mask for non-central acceleration lines.
+
         Args:
             num_cols: Number of columns of k-space (2D subsampling).
             acceleration: Desired acceleration rate.
             offset: Offset from 0 to begin masking (for equispaced masks).
             num_low_frequencies: Integer count of low-frequency lines sampled.
+
         Returns:
             A mask for the high spatial frequencies of k-space.
         """
@@ -169,9 +180,11 @@ class MaskFunc:
     ) -> np.ndarray:
         """
         Build center mask based on number of low frequencies.
+
         Args:
             shape: Shape of k-space to mask.
             num_low_freqs: Number of low-frequency lines to sample.
+
         Returns:
             A mask for hte low spatial frequencies of k-space.
         """
@@ -197,6 +210,7 @@ class MaskFunc:
 class RandomMaskFunc(MaskFunc):
     """
     Creates a random sub-sampling mask of a given shape.
+
     The mask selects a subset of columns from the input k-space data. If the
     k-space data has N columns, the mask picks out:
         1. N_low_freqs = (N * center_fraction) columns in the center
@@ -205,9 +219,11 @@ class RandomMaskFunc(MaskFunc):
         probability equal to: prob = (N / acceleration - N_low_freqs) /
         (N - N_low_freqs). This ensures that the expected number of columns
         selected is equal to (N / acceleration).
+
     It is possible to use multiple center_fractions and accelerations, in which
     case one possible (center_fraction, acceleration) is chosen uniformly at
     random each time the ``RandomMaskFunc`` object is called.
+
     For example, if accelerations = [4, 8] and center_fractions = [0.08, 0.04],
     then there is a 50% probability that 4-fold acceleration with 8% center
     fraction is selected and a 50% probability that 8-fold acceleration with 4%
@@ -231,6 +247,7 @@ class RandomMaskFunc(MaskFunc):
 class EquiSpacedMaskFunc(MaskFunc):
     """
     Sample data with equally-spaced k-space lines.
+
     The lines are spaced exactly evenly, as is done in standard GRAPPA-style
     acquisitions. This means that with a densely-sampled center,
     ``acceleration`` will be greater than the true acceleration rate.
@@ -245,12 +262,14 @@ class EquiSpacedMaskFunc(MaskFunc):
     ) -> np.ndarray:
         """
         Produce mask for non-central acceleration lines.
+
         Args:
             num_cols: Number of columns of k-space (2D subsampling).
             acceleration: Desired acceleration rate.
             offset: Offset from 0 to begin masking. If no offset is specified,
                 then one is selected randomly.
             num_low_frequencies: Not used.
+
         Returns:
             A mask for the high spatial frequencies of k-space.
         """
@@ -266,6 +285,7 @@ class EquiSpacedMaskFunc(MaskFunc):
 class EquispacedMaskFractionFunc(MaskFunc):
     """
     Equispaced mask with exact acceleration matching.
+
     The mask selects a subset of columns from the input k-space data. If the
     k-space data has N columns, the mask picks out:
         1. N_low_freqs = (N * center_fraction) columns in the center
@@ -274,9 +294,11 @@ class EquispacedMaskFractionFunc(MaskFunc):
            that reaches the desired acceleration rate taking into consideration
            the number of low frequencies. This ensures that the expected number
            of columns selected is equal to (N / acceleration)
+
     It is possible to use multiple center_fractions and accelerations, in which
     case one possible (center_fraction, acceleration) is chosen uniformly at
     random each time the EquispacedMaskFunc object is called.
+
     Note that this function may not give equispaced samples (documented in
     https://github.com/facebookresearch/fastMRI/issues/54), which will require
     modifications to standard GRAPPA approaches. Nonetheless, this aspect of
@@ -292,6 +314,7 @@ class EquispacedMaskFractionFunc(MaskFunc):
     ) -> np.ndarray:
         """
         Produce mask for non-central acceleration lines.
+
         Args:
             num_cols: Number of columns of k-space (2D subsampling).
             acceleration: Desired acceleration rate.
@@ -299,6 +322,7 @@ class EquispacedMaskFractionFunc(MaskFunc):
                 then one is selected randomly.
             num_low_frequencies: Number of low frequencies. Used to adjust mask
                 to exactly match the target acceleration.
+
         Returns:
             A mask for the high spatial frequencies of k-space.
         """
@@ -320,14 +344,18 @@ class EquispacedMaskFractionFunc(MaskFunc):
 class MagicMaskFunc(MaskFunc):
     """
     Masking function for exploiting conjugate symmetry via offset-sampling.
+
     This function applies the mask described in the following paper:
+
     Defazio, A. (2019). Offset Sampling Improves Deep Learning based
     Accelerated MRI Reconstructions by Exploiting Symmetry. arXiv preprint,
     arXiv:1912.01101.
+
     It is essentially an equispaced mask with an offset for the opposite site
     of k-space. Since MRI images often exhibit approximate conjugate k-space
     symmetry, this mask is generally more efficient than a standard equispaced
     mask.
+
     Similarly to ``EquispacedMaskFunc``, this mask will usually undereshoot the
     target acceleration rate.
     """
@@ -341,12 +369,14 @@ class MagicMaskFunc(MaskFunc):
     ) -> np.ndarray:
         """
         Produce mask for non-central acceleration lines.
+
         Args:
             num_cols: Number of columns of k-space (2D subsampling).
             acceleration: Desired acceleration rate.
             offset: Offset from 0 to begin masking. If no offset is specified,
                 then one is selected randomly.
             num_low_frequencies: Not used.
+
         Returns:
             A mask for the high spatial frequencies of k-space.
         """
@@ -377,14 +407,18 @@ class MagicMaskFunc(MaskFunc):
 class MagicMaskFractionFunc(MagicMaskFunc):
     """
     Masking function for exploiting conjugate symmetry via offset-sampling.
+
     This function applies the mask described in the following paper:
+
     Defazio, A. (2019). Offset Sampling Improves Deep Learning based
     Accelerated MRI Reconstructions by Exploiting Symmetry. arXiv preprint,
     arXiv:1912.01101.
+
     It is essentially an equispaced mask with an offset for the opposite site
     of k-space. Since MRI images often exhibit approximate conjugate k-space
     symmetry, this mask is generally more efficient than a standard equispaced
     mask.
+
     Similarly to ``EquispacedMaskFractionFunc``, this method exactly matches
     the target acceleration by adjusting the offsets.
     """
@@ -396,13 +430,16 @@ class MagicMaskFractionFunc(MagicMaskFunc):
     ) -> Tuple[torch.Tensor, torch.Tensor, int]:
         """
         Sample a new k-space mask.
+
         This function samples and returns two components of a k-space mask: 1)
         the center mask (e.g., for sensitivity map calculation) and 2) the
         acceleration mask (for the edge of k-space). Both of these masks, as
         well as the integer of low frequency samples, are returned.
+
         Args:
             shape: Shape of the k-space to subsample.
             offset: Offset from 0 to begin mask (for equispaced masks).
+
         Returns:
             A 3-tuple contaiing 1) the mask for the center of k-space, 2) the
             mask for the high frequencies of k-space, and 3) the integer count
@@ -445,9 +482,11 @@ def create_mask_for_mask_type(
 ) -> MaskFunc:
     """
     Creates a mask of the specified type.
+
     Args:
         center_fractions: What fraction of the center of k-space to include.
         accelerations: What accelerations to apply.
+
     Returns:
         A mask func for the target mask type.
     """

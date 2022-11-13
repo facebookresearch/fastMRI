8f6b7bc971bc91bde397394042fa54605d0bbda9
Matthew Muckley <matt.muckley@gmail.com>
2022-11-13 15:44:56 -0800

    Add ZSNet implementation

--- fastmri/models/__init__.py
+++ fastmri/models/__init__.py
@@ -5,6 +5,7 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+from ._zsnet import ZSNet
 from .adaptive_varnet import AdaptiveVarNet
 from .policy import StraightThroughPolicy
 from .unet import Unet
--- fastmri/models/_zsnet.py
+++ fastmri/models/_zsnet.py
@@ -14,14 +14,20 @@ import torch.nn.functional as F
 
 import fastmri
 from fastmri.data import transforms
+from fastmri.models.varnet import SensitivityModel
 
-from .unet import Unet
+from ._zsnet_unet import ZSNetUnet
+
+
+def _create_zero_tensor(tensor_type: torch.Tensor) -> torch.Tensor:
+    return torch.zeros(
+        (1, 1, 1, 1, 1), dtype=tensor_type.dtype, device=tensor_type.device
+    )
 
 
 class NormUnet(nn.Module):
     """
     Normalized U-Net model.
-
     This is the same as a regular U-Net, but with normalization applied to the
     input before the U-Net. This keeps the values more numerically stable
     during training.
@@ -45,7 +51,7 @@ class NormUnet(nn.Module):
         """
         super().__init__()
 
-        self.unet = Unet(
+        self.unet = ZSNetUnet(
             in_chans=in_chans,
             out_chans=out_chans,
             chans=chans,
@@ -55,8 +61,7 @@ class NormUnet(nn.Module):
 
     def complex_to_chan_dim(self, x: torch.Tensor) -> torch.Tensor:
         b, c, h, w, two = x.shape
-        assert two == 2
-        return x.permute(0, 4, 1, 2, 3).reshape(b, 2 * c, h, w)
+        return x.permute(0, 4, 1, 2, 3).reshape(b, two * c, h, w)
 
     def chan_complex_to_last_dim(self, x: torch.Tensor) -> torch.Tensor:
         b, c2, h, w = x.shape
@@ -67,10 +72,22 @@ class NormUnet(nn.Module):
     def norm(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         # group norm
         b, c, h, w = x.shape
-        x = x.view(b, 2, c // 2 * h * w)
 
-        mean = x.mean(dim=2).view(b, 2, 1, 1)
-        std = x.std(dim=2).view(b, 2, 1, 1)
+        x = x.contiguous().view(b, c, c // c * h * w)
+        mean = (
+            x.mean(dim=2)
+            .view(b, c, 1, 1, 1)
+            .expand(b, c, c // c, 1, 1)
+            .contiguous()
+            .view(b, c, 1, 1)
+        )
+        std = (
+            x.std(dim=2)
+            .view(b, c, 1, 1, 1)
+            .expand(b, c, c // c, 1, 1)
+            .contiguous()
+            .view(b, c, 1, 1)
+        )
 
         x = x.view(b, c, h, w)
 
@@ -79,6 +96,10 @@ class NormUnet(nn.Module):
     def unnorm(
         self, x: torch.Tensor, mean: torch.Tensor, std: torch.Tensor
     ) -> torch.Tensor:
+        if not mean.shape[1] == x.shape[1]:
+            mean = mean[:, :2]
+        if not std.shape[1] == x.shape[1]:
+            std = std[:, :2]
         return x * std + mean
 
     def pad(
@@ -93,6 +114,7 @@ class NormUnet(nn.Module):
         # the documentation lies - this actually takes a list
         # https://github.com/pytorch/pytorch/blob/master/torch/nn/functional.py#L3457
         # https://github.com/pytorch/pytorch/pull/16949
+
         x = F.pad(x, w_pad + h_pad)
 
         return x, (h_pad, w_pad, h_mult, w_mult)
@@ -108,33 +130,22 @@ class NormUnet(nn.Module):
         return x[..., h_pad[0] : h_mult - h_pad[1], w_pad[0] : w_mult - w_pad[1]]
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        if not x.shape[-1] == 2:
-            raise ValueError("Last dimension must be 2 for complex.")
-
         # get shapes for unet and normalize
         x = self.complex_to_chan_dim(x)
-        x, mean, std = self.norm(x)
         x, pad_sizes = self.pad(x)
+        x, mean, std = self.norm(x)
 
-        x = self.unet(x)
+        x = self.unet(x.contiguous())
 
         # get shapes back and unnormalize
-        x = self.unpad(x, *pad_sizes)
         x = self.unnorm(x, mean, std)
+        x = self.unpad(x, *pad_sizes)
         x = self.chan_complex_to_last_dim(x)
 
         return x
 
 
-class SensitivityModel(nn.Module):
-    """
-    Model for learning sensitivity estimation from k-space data.
-
-    This model applies an IFFT to multichannel k-space data and then a U-Net
-    to the coil images to estimate coil sensitivities. It can be used with the
-    end-to-end variational network.
-    """
-
+class ZSNetSensitivityModel(SensitivityModel):
     def __init__(
         self,
         chans: int,
@@ -144,19 +155,9 @@ class SensitivityModel(nn.Module):
         drop_prob: float = 0.0,
         mask_center: bool = True,
     ):
-        """
-        Args:
-            chans: Number of output channels of the first convolution layer.
-            num_pools: Number of down-sampling and up-sampling layers.
-            in_chans: Number of channels in the input to the U-Net model.
-            out_chans: Number of channels in the output to the U-Net model.
-            drop_prob: Dropout probability.
-            mask_center: Whether to mask center of k-space for sensitivity map
-                calculation.
-        """
-        super().__init__()
-        self.mask_center = mask_center
-        self.norm_unet: nn.Module = NormUnet(
+        super().__init__(chans, num_pools, in_chans, out_chans, drop_prob, mask_center)
+        # overwrite unet
+        self.norm_unet = NormUnet(
             chans,
             num_pools,
             in_chans=in_chans,
@@ -164,81 +165,17 @@ class SensitivityModel(nn.Module):
             drop_prob=drop_prob,
         )
 
-    def chans_to_batch_dim(self, x: torch.Tensor) -> Tuple[torch.Tensor, int]:
-        b, c, h, w, comp = x.shape
 
-        return x.view(b * c, 1, h, w, comp), b
-
-    def batch_chans_to_chan_dim(self, x: torch.Tensor, batch_size: int) -> torch.Tensor:
-        bc, _, h, w, comp = x.shape
-        c = bc // batch_size
-
-        return x.view(batch_size, c, h, w, comp)
-
-    def divide_root_sum_of_squares(self, x: torch.Tensor) -> torch.Tensor:
-        return x / fastmri.rss_complex(x, dim=1).unsqueeze(-1).unsqueeze(1)
-
-    def get_pad_and_num_low_freqs(
-        self, mask: torch.Tensor, num_low_frequencies: Optional[int] = None
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        if num_low_frequencies is None or num_low_frequencies == 0:
-            # get low frequency line locations and mask them out
-            squeezed_mask = mask[:, 0, 0, :, 0].to(torch.int8)
-            cent = squeezed_mask.shape[1] // 2
-            # running argmin returns the first non-zero
-            left = torch.argmin(squeezed_mask[:, :cent].flip(1), dim=1)
-            right = torch.argmin(squeezed_mask[:, cent:], dim=1)
-            num_low_frequencies_tensor = torch.max(
-                2 * torch.min(left, right), torch.ones_like(left)
-            )  # force a symmetric center unless 1
-        else:
-            num_low_frequencies_tensor = num_low_frequencies * torch.ones(
-                mask.shape[0], dtype=mask.dtype, device=mask.device
-            )
-
-        pad = (mask.shape[-2] - num_low_frequencies_tensor + 1) // 2
-
-        return pad, num_low_frequencies_tensor
-
-    def forward(
-        self,
-        masked_kspace: torch.Tensor,
-        mask: torch.Tensor,
-        num_low_frequencies: Optional[int] = None,
-    ) -> torch.Tensor:
-        if self.mask_center:
-            pad, num_low_freqs = self.get_pad_and_num_low_freqs(
-                mask, num_low_frequencies
-            )
-            masked_kspace = transforms.batched_mask_center(
-                masked_kspace, pad, pad + num_low_freqs
-            )
-
-        # convert to image space
-        images, batches = self.chans_to_batch_dim(fastmri.ifft2c(masked_kspace))
-
-        # estimate sensitivities
-        return self.divide_root_sum_of_squares(
-            self.batch_chans_to_chan_dim(self.norm_unet(images), batches)
-        )
-
-
-class VarNet(nn.Module):
-    """
-    A full variational network model.
-
-    This model applies a combination of soft data consistency with a U-Net
-    regularizer. To use non-U-Net regularizers, use VarNetBlock.
-    """
-
+class ZSNet(nn.Module):
     def __init__(
         self,
-        num_cascades: int = 12,
-        sens_chans: int = 8,
-        sens_pools: int = 4,
-        chans: int = 18,
-        pools: int = 4,
-        mask_center: bool = True,
+        image_crop_size: int,
+        num_concat_cascades: int = 18,
+        sens_chans: int = 10,
+        sens_pools: int = 5,
+        chans: int = 20,
+        pools: int = 5,
+        mask_center: bool = False,
     ):
         """
         Args:
@@ -250,45 +187,49 @@ class VarNet(nn.Module):
             chans: Number of channels for cascade U-Net.
             pools: Number of downsampling and upsampling layers for cascade
                 U-Net.
-            mask_center: Whether to mask center of k-space for sensitivity map
-                calculation.
         """
         super().__init__()
 
-        self.sens_net = SensitivityModel(
-            chans=sens_chans,
-            num_pools=sens_pools,
-            mask_center=mask_center,
+        self.sens_net = ZSNetSensitivityModel(
+            sens_chans, sens_pools, mask_center=mask_center
         )
-        self.cascades = nn.ModuleList(
-            [VarNetBlock(NormUnet(chans, pools)) for _ in range(num_cascades)]
-        )
+        self.init_layer = ZSNetBlock(NormUnet(chans, pools), crop_size=image_crop_size)
+        self.cascades = nn.ModuleList()
+        for i in range(1, num_concat_cascades):
+            chan_mult = 2
+            self.cascades.append(
+                ZSNetConcatBlock(
+                    NormUnet(chans=chans, num_pools=pools, in_chans=2 * chan_mult),
+                    crop_size=image_crop_size,
+                )
+            )
 
     def forward(
         self,
         masked_kspace: torch.Tensor,
         mask: torch.Tensor,
         num_low_frequencies: Optional[int] = None,
+        only_center: bool = False,
     ) -> torch.Tensor:
         sens_maps = self.sens_net(masked_kspace, mask, num_low_frequencies)
-        kspace_pred = masked_kspace.clone()
-
+        kspace_pred, image = self.init_layer(
+            masked_kspace.clone(), masked_kspace, mask, sens_maps
+        )
+        previous_images = [image]
         for cascade in self.cascades:
-            kspace_pred = cascade(kspace_pred, masked_kspace, mask, sens_maps)
+            kspace_pred, image = cascade(
+                kspace_pred,
+                masked_kspace,
+                mask,
+                sens_maps,
+                previous_images,
+            )
 
         return fastmri.rss(fastmri.complex_abs(fastmri.ifft2c(kspace_pred)), dim=1)
 
 
-class VarNetBlock(nn.Module):
-    """
-    Model block for end-to-end variational network.
-
-    This model applies a combination of soft data consistency with the input
-    model as a regularizer. A series of these blocks can be stacked to form
-    the full variational network.
-    """
-
-    def __init__(self, model: nn.Module):
+class ZSNetBaseBlock(nn.Module):
+    def __init__(self, model: nn.Module, crop_size: int):
         """
         Args:
             model: Module for "regularization" component of variational
@@ -297,6 +238,7 @@ class VarNetBlock(nn.Module):
         super().__init__()
 
         self.model = model
+        self.crop_size = crop_size
         self.dc_weight = nn.Parameter(torch.ones(1))
 
     def sens_expand(self, x: torch.Tensor, sens_maps: torch.Tensor) -> torch.Tensor:
@@ -307,17 +249,71 @@ class VarNetBlock(nn.Module):
             fastmri.ifft2c(x), fastmri.complex_conj(sens_maps)
         ).sum(dim=1, keepdim=True)
 
+    def image_crop(self, image: torch.Tensor) -> torch.Tensor:
+        input_shape = image.shape
+        crop_size = (min(self.crop_size, input_shape[-3]), input_shape[-2])
+
+        return transforms.complex_center_crop(image, crop_size)
+
+    def image_uncrop(
+        self, image: torch.Tensor, original_image: torch.Tensor
+    ) -> torch.Tensor:
+        """Insert values back into original image."""
+        in_shape = original_image.shape
+        crop_height = image.shape[-3]
+        in_height = in_shape[-3]
+        pad_height = (in_height - crop_height) // 2
+        if (in_height - crop_height) % 2 != 0:
+            pad_height_top = pad_height + 1
+        else:
+            pad_height_top = pad_height
+
+        original_image[..., pad_height_top:-pad_height, :, :] = image[...]  # type: ignore
+
+        return original_image
+
+    def apply_model_with_crop(self, image: torch.Tensor) -> torch.Tensor:
+        if self.crop_size is not None:
+            image = self.image_uncrop(
+                self.model(self.image_crop(image)), image[..., :2].clone()
+            )
+        else:
+            image = self.model(image)
+
+        return image
+
+
+class ZSNetBlock(ZSNetBaseBlock):
     def forward(
         self,
         current_kspace: torch.Tensor,
         ref_kspace: torch.Tensor,
         mask: torch.Tensor,
         sens_maps: torch.Tensor,
-    ) -> torch.Tensor:
-        zero = torch.zeros(1, 1, 1, 1, 1).to(current_kspace)
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        zero = _create_zero_tensor(current_kspace)
         soft_dc = torch.where(mask, current_kspace - ref_kspace, zero) * self.dc_weight
-        model_term = self.sens_expand(
-            self.model(self.sens_reduce(current_kspace, sens_maps)), sens_maps
-        )
+        image = self.sens_reduce(current_kspace, sens_maps)
+        model_term = self.sens_expand(self.apply_model_with_crop(image), sens_maps)
 
-        return current_kspace - soft_dc - model_term
+        return current_kspace - soft_dc - model_term, image
+
+
+class ZSNetConcatBlock(ZSNetBaseBlock):
+    def forward(
+        self,
+        current_kspace: torch.Tensor,
+        ref_kspace: torch.Tensor,
+        mask: torch.Tensor,
+        sens_maps: torch.Tensor,
+        previous_images: List[torch.Tensor],
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        zero = _create_zero_tensor(current_kspace)
+        soft_dc = torch.where(mask, current_kspace - ref_kspace, zero) * self.dc_weight
+        image = self.sens_reduce(current_kspace, sens_maps)
+
+        model_term = self.sens_expand(
+            self.apply_model_with_crop(torch.cat([image] + previous_images, dim=-1)),
+            sens_maps,
+        )
+        return current_kspace - soft_dc - model_term, image

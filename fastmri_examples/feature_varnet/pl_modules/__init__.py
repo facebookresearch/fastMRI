c0ba931ddbc22eed0837acc1739ca5f20bf0be8a
GiannakopoulosIlias <iliasgiannako@gmail.com>
2024-06-12 13:29:33 -0400

    Included copyright header and fixed linter tests

--- fastmri_examples/feature_varnet/pl_modules/__init__.py
+++
@@ -1,9 +0,0 @@
-
-"""
-Copyright (c) Facebook, Inc. and its affiliates.
-
-This source code is licensed under the MIT license found in the
-LICENSE file in the root directory of this source tree.
-"""
-
-from .feature_varnet_module import FIVarNetModule
